5380a83bb6edec74e7dabb2caec185a7d519bae6
kobanago <kobanago3800@gmail.com>
2024-04-29 13:36:03 +0900

    chore：eslint：fix

--- eslintrc.cjs
+++ eslintrc.cjs
@@ -1,50 +1,56 @@
 /** @type {import('eslint').ESLint.ConfigData} */
-
 module.exports = {
-  "root": true,
-  "env": {
-      "browser": true,
-      "es2021": true,
-      "node": true,
-  },
-
-  extends: [
-    "plugin:vue/vue3-essential",
-    "plugin:vue/vue3-recommended",
-    "eslint:recommended",
-    "@vue/eslint-config-airbnb-with-typescript",
-    "@vue/eslint-config-prettier",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-
-  parserOptions: {
-    ecmaVersion: "2021",
-  },
-
-  rules: {
-    "vue/component-tags-order": [
-      "error",
-      {
-        order: ["script", "template", "style"],
-      },
-    ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/typedef": [
-      "error",
-      {
-        variableDeclaration: true,
-        memberVariableDeclaration: true,
-        propertyDeclaration: true,
-        variableDeclarationIgnoreFunction: true,
-        arrayDestructuring: true,
-        objectDestructuring: true,
-        parameter: true,
-        arrowParameter: true,
-      },
-    ],
-  },
+	root: true,
+	env: {
+		browser: true,
+		es2022: true,
+		node: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:vue/vue3-essential",
+		"plugin:vue/vue3-recommended",
+		"plugin:@typescript-eslint/recommended",
+		"@vue/eslint-config-airbnb-with-typescript",
+		"@vue/eslint-config-prettier",
+		"@vue/typescript/recommended",
+		"@vue/prettier",
+		"@vue/prettier/@typescript-eslint",
+	],
+	// parser: "vue-eslint-parser",
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: "module",
+		ecmaVersion: "latest",
+		vueFeatures: {
+			filter: true,
+			interpolationAsNonHTML: false,
+		},
+	},
+	plugins: ["vue", "@typescript-eslint"],
+	rules: {
+		"vue/component-tags-order": [
+			"error",
+			{
+				order: ["script", "template", "style"],
+			},
+		],
+		"@typescript-eslint/no-explicit-any": "error",
+		"@typescript-eslint/typedef": [
+			"error",
+			{
+				variableDeclaration: true,
+				memberVariableDeclaration: true,
+				propertyDeclaration: true,
+				variableDeclarationIgnoreFunction: true,
+				arrayDestructuring: true,
+				objectDestructuring: true,
+				parameter: true,
+				arrowParameter: true,
+			},
+		],
+	},
 };
-
-
