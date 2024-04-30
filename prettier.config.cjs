47a1577cc61dc8fd45eebb54940f74497bd2333b
kobanago <kobanago3800@gmail.com>
2024-04-30 12:31:18 +0900

    chore：format：configs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,10 +1,10 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-	semi: true, // セミコロン有
-	singleQuote: true, // シングルクォート使う
-	printWidth: 100, // 折り返し
-	tabWidth: 2, // スペースの単位
-	trailingComma: "all", // ケツカンマつける
-	jsxSingleQuote: true, //　JSXでシングルクォートを使用
-	endOfLine: "lf", //LF
+  semi: true, // セミコロン有
+  singleQuote: true, // シングルクォート使う
+  printWidth: 100, // 折り返し
+  tabWidth: 2, // スペースの単位
+  trailingComma: 'all', // ケツカンマつける
+  jsxSingleQuote: true, // JSXでシングルクォートを使用
+  endOfLine: 'lf', //LF
 };
